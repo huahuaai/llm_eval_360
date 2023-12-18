c572bf3b20da51301854f931740628fa5f5a4d8a
hhai <924024667@qq.com>
2023-12-18 12:43:21 +0800

    add model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
+import argparse
 from evaluator import Evaluator
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("")
+    parser.add_argument("--device_map",)
+    parser.add_argument()
+    args = parser.parse_args()
 
-
+def main():
+    args = get_args()
 
 
 if __name__ == '__main__':
-    x=0
+    main()
--- src/models/models.py
+++ src/models/models.py
@@ -1,10 +1,39 @@
+from typing import Any
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from templates import get_template
 
+#first complete the version without accelerte
 class LLM():
     def __init__(
         self,
-
+        model_name_or_path: str,
+        use_accelerate: bool=False,
+        device_map: str='auto',
+        load_in_8bit: bool=False,
+        load_in_4bit: bool=False
     ):
+        self.model = self.create_model(
+
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path,
+            device_map=device_map,
+            load_in_4bit=load_in_4bit,
+            load_in_8bit=load_in_8bit
+        )
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+    def generate_oneturn(self):
         x=0
-        
+    def decode_token_to_text(self):
+        x=0
+    def tokenize_text_to_token(self):
+        x=0
+    def get_prompt(self):
+        x=0
+    def create_model(self):
+        x=0
+    
